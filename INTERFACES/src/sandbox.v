a7acf6b0c3d95549859b8446d7b41a03a6324506
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2016-03-03 22:44:37 +0300

    Start HW CAN

--- INTERFACES/src/sandbox.v
+++ INTERFACES/src/sandbox.v
@@ -29,62 +29,51 @@ module sandbox
     input wire GCLK,
     output wire [7:0] LD,
     input wire [7:0] SW,
-    output wire [7:0] JA,
-    input wire [7:0] JB,
+    inout wire [7:0] JA,
+    inout wire [7:0] JB,
     input wire BTNC,
     input wire BTND,
     input wire BTNL,
     input wire BTNR,
     input wire BTNU
     );
-    
-    wire MISO;
-    wire MOSI;
-    wire SS;
-    wire SCLK;
-    
-    reg SS_REG = 1'b0;
-    reg SCLK_REG = 1'b0;
-    assign JA[2] = SS_REG;
-    assign JA[3] = SCLK_REG;
-    
-    always @(posedge GCLK) begin
-        SS_REG <= SS;
-        SCLK_REG <= SCLK;
-    end
-    
-    //assign JA[3] = SCLK;
-   
-    SPI #(.m(15), .Tbit(100)) spi
-        (
-        // External interfaces
-        .str0(OLED_S0),
-        .str1(OLED_S1),
-        .str2(OLED_S2),
-        .str3(OLED_S3),
-        .GCLK(GCLK),
-        .RST(BTND),
-        .SW(SW),
-        // Transmission start switch
-        .st(BTNC),
-        // SPI Master bus
-        //.MASTER_MISO(MISO),
-        //.MASTER_MOSI(MOSI),
-        .MASTER_SS(SS),
-        .MASTER_SCLK(SCLK),
-        .MASTER_MISO(JB[0]),
-        .MASTER_MOSI(JA[1]),
-        //.MASTER_SS(JA[2]),
-        //.MASTER_SCLK(JA[3]),
-        // SPI Slave bus
-        //.SLAVE_MISO(MISO),
-        //.SLAVE_MOSI(MOSI),
-        .SLAVE_SS(SS),
-        .SLAVE_SCLK(SCLK),
-        .SLAVE_MOSI(JB[1]),
-        .SLAVE_MISO(JA[0])
-        //.SLAVE_SS(JB[2])
-        //.SLAVE_SCLK(JB[3])
-        );
 
+    reg [107:0] DIN1;
+    reg [107:0] DIN2;
+    wire [107:0] DOUT1;
+    wire [107:0] DOUT2;
+
+    reg tx_start1;
+    wire tx_ready1;
+    wire rx_ready1;
+    reg tx_start2;
+    wire tx_ready2;
+    wire rx_ready2;
+
+    can_controller CC1
+    (
+        .GCLK(GCLK),
+        .RES(RES),
+        .CAN(CAN),
+        .DIN(DIN1),
+        .DOUT(DOUT1),
+        .tx_start(tx_start1),
+        .tx_ready(tx_ready1),
+        .rx_ready(rx_ready1)
+    );
+
+    can_controller CC2
+    (
+        .GCLK(GCLK),
+        .RES(RES),
+        .CAN(CAN),
+        .DIN(DIN1),
+        .DOUT(DOUT1),
+        .tx_start(tx_start1),
+        .tx_ready(tx_ready1),
+        .rx_ready(rx_ready1)
+    );    
+    
+    
+    
 endmodule
