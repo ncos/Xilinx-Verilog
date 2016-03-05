d48ecb362b9911ea652557e55ab62b22f189c494
Alexandra Tsvetkova <sash.tsvet@gmail.com>
2016-03-06 01:24:34 +0300

    CAN in HW

--- INTERFACES/src/sandbox.v
+++ INTERFACES/src/sandbox.v
@@ -20,14 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module sandbox
+module sandbox#
     (
-    output wire [127:0] OLED_S0,
-    output wire [127:0] OLED_S1,
-    output wire [127:0] OLED_S2,
-    output wire [127:0] OLED_S3,
+    parameter integer Tbit = 100 // Clocks
+    )
+    (
+    output reg [127:0] OLED_S0,
+    output reg [127:0] OLED_S1,
+    output reg [127:0] OLED_S2,
+    output reg [127:0] OLED_S3,
     input wire GCLK,
-    output wire [7:0] LD,
+    output reg [7:0] LD,
     input wire [7:0] SW,
     inout wire [7:0] JA,
     inout wire [7:0] JB,
@@ -38,15 +41,13 @@ module sandbox
     input wire BTNU
     );
 
-    reg [107:0] DIN1;
-    reg [107:0] DIN2;
+    reg [107:0] DIN1 = "HI";
+    reg [107:0] DIN2 = "LOL";
     wire [107:0] DOUT1;
     wire [107:0] DOUT2;
 
-    reg tx_start1;
     wire tx_ready1;
     wire rx_ready1;
-    reg tx_start2;
     wire tx_ready2;
     wire rx_ready2;
 
@@ -54,10 +55,10 @@ module sandbox
     (
         .GCLK(GCLK),
         .RES(RES),
-        .CAN(CAN),
+        .CAN(JA[0]),
         .DIN(DIN1),
         .DOUT(DOUT1),
-        .tx_start(tx_start1),
+        .tx_start(BTNC),
         .tx_ready(tx_ready1),
         .rx_ready(rx_ready1)
     );
@@ -66,14 +67,29 @@ module sandbox
     (
         .GCLK(GCLK),
         .RES(RES),
-        .CAN(CAN),
-        .DIN(DIN1),
-        .DOUT(DOUT1),
-        .tx_start(tx_start1),
-        .tx_ready(tx_ready1),
-        .rx_ready(rx_ready1)
+        .CAN(JA[1]),
+        .DIN(DIN2),
+        .DOUT(DOUT2),
+        .tx_start(BTNC),
+        .tx_ready(tx_ready2),
+        .rx_ready(rx_ready2)
     );    
-    
-    
+       
+    CLK_DIV clk_div
+    (
+        .GCLK(GCLK),
+        .out(clk_Tbit),
+        .T(Tbit)
+    );
+    always @(posedge clk_Tbit) begin
+            OLED_S0 <= "CAN Interface   ";
+            OLED_S1 <= {DOUT1,20'd0}; 
+            OLED_S2 <= {DOUT2,20'd0};
+            OLED_S3 <= "CAN Interface   ";
+            LD[0] <= tx_ready1;
+            LD[1] <= rx_ready1;
+            LD[2] <= tx_ready2;
+            LD[3] <= rx_ready2;
+    end    
     
 endmodule
